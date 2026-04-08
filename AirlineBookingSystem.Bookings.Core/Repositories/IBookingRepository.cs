322254a2dbe2aa2b39700a36e531c912bf55c4b9
Guilherme Monção <guilherme_oliveira27@outlook.com>
2026-04-07 20:54:02 -0400

    Feat: Criação de Repositórios

---
+++ AirlineBookingSystem.Bookings.Core/Repositories/IBookingRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem.Bookings.Core.Repositories {
+    public interface IBookingRepository {
+        Task<Booking> GetBookingByIdAsync(Guid id);
+        Task AddBookingAsync(Booking booking);
+    }
+}
---
+++ AirlineBookingSystem.Flights.Core/Repositories/IFlightRepository.cs
@@ -0,0 +1,14 @@
+﻿using AirlineBookingSystem.Bookings.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem.Flights.Core.Repositories {
+    public interface IFlightRepository {
+        Task<IEnumerable<Flight>> GetFlightAsync();
+        Task AddFlightAsync(Flight flight);
+        Task DeleteFlightAsync(Guid id);
+    }
+}
